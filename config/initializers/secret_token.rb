98463f224833c1a68aceb406cb09c4d513d0da79
Ian Irving <ian@falsepositives.com>
2013-08-19 16:48:50 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestHarnessR24::Application.config.secret_key_base = '87d608a4762dff5b9ffe65256893eb0ec80832421989d7698d3b01e1a52b1eded3bb64cc2ee44996245c619bdf2e3f316f2cd36134e405b1cec5936fe6903792'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestHarnessR24::Application.config.session_store :cookie_store, key: '_test-harness-r24_session'
